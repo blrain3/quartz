aab5ff6866e90fecbd4065208569da88de810a2f
blrain <rain.view@outlook.com>
2026-07-27 11:26:13 +0800

    更新配置文件，修改页面标题和颜色主题；重构页脚组件，移除不必要的元素

--- quartz/components/MyFooter.tsx
+++ quartz/components/MyFooter.tsx
@@ -2,16 +2,8 @@ import type { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 export default (() => {
   const MyFooter: QuartzComponent = () => {
-    const year = new Date().getFullYear()
     return (
-      <footer>
-        <img
-          src="https://tg.salix.eu.org/file/1784560281480_image.webp"
-          alt="footer image"
-          class="footer-img"
-        />
-        <p>blrain &copy; {year}</p>
-      </footer>
+      <footer></footer>
     )
   }
 
@@ -20,12 +12,6 @@ export default (() => {
       text-align: center;
       margin-bottom: 4rem;
       opacity: 0.7;
-    }
-    footer .footer-img {
-      max-width: 100%;
-      height: auto;
-      display: block;
-      margin: 0 auto 0.5rem;
     }
   `
 
