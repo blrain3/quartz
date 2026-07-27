c335e46fdc07725bc205835df980ead515259e69
blrain <rain.view@outlook.com>
2026-07-27 17:27:50 +0800

    移除 MyFooter 组件中的 footer 元素，返回 null；删除不再使用的图标路径；新增图标文件。

--- quartz/components/MyFooter.tsx
+++ quartz/components/MyFooter.tsx
@@ -2,18 +2,8 @@ import type { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 export default (() => {
   const MyFooter: QuartzComponent = () => {
-    return (
-      <footer></footer>
-    )
+    return null
   }
-
-  MyFooter.css = `
-    footer {
-      text-align: center;
-      margin-bottom: 4rem;
-      opacity: 0.7;
-    }
-  `
 
   return MyFooter
 }) satisfies QuartzComponentConstructor
