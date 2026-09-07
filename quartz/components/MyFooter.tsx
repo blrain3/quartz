e982ba5c4f087efc659f5eff8ae7b130296455da
blrain <helloblrain@gmail.com>
2026-09-07 08:52:57 +0800

    增加异次元之旅链接到页脚

--- quartz/components/MyFooter.tsx
+++ quartz/components/MyFooter.tsx
@@ -8,6 +8,9 @@ export default (() => {
           <a href="https://icp.gov.moe/?keyword=20260697" target="_blank" rel="noopener noreferrer">
             萌ICP备20260697号
           </a>
+          <a style="text-decoration:none;color:#55bb8a;" href="https://travel.moe/go.html" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">
+            <img src="https://travel.moe/images/icon/icon64green.png" style="width:24px;height:24px" alt="异次元之旅">异次元之旅
+          </a>
         </p>
       </footer>
     )
