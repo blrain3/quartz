9343c368593b6d2b59337e5f6fc7f7995625a8c1
blrain <helloblrain@gmail.com>
2026-09-07 09:24:13 +0800

    更新首页内容，添加“开往”链接到页脚，并优化页脚链接的间距样式

--- quartz/components/MyFooter.tsx
+++ quartz/components/MyFooter.tsx
@@ -11,6 +11,9 @@ export default (() => {
           <a href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">
             异次元之旅
           </a>
+          <a href="https://www.travellings.cn/go.html" target="_blank" rel="noopener" title="开往-友链接力">
+            开往
+          </a>
         </p>
       </footer>
     )
