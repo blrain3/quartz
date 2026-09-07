2f3fe164af040d7b9e6a7ebd2cd445b09a41ac27
blrain <helloblrain@gmail.com>
2026-09-07 18:03:52 +0800

    chore: trim unused Quartz docs and align content builds

--- quartz/components/MyFooter.tsx
+++ quartz/components/MyFooter.tsx
@@ -8,10 +8,19 @@ export default (() => {
           <a href="https://icp.gov.moe/?keyword=20260697" target="_blank" rel="noopener noreferrer">
             萌ICP备20260697号
           </a>
-          <a href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">
+          <a
+            href="https://travel.moe/go.html?travel=on"
+            title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！"
+            target="_blank"
+          >
             异次元之旅
           </a>
-          <a href="https://www.travellings.cn/go.html" target="_blank" rel="noopener" title="开往-友链接力">
+          <a
+            href="https://www.travellings.cn/go.html"
+            target="_blank"
+            rel="noopener"
+            title="开往-友链接力"
+          >
             开往
           </a>
         </p>
